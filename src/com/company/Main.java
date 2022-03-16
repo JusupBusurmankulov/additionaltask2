78a6d09b3c299fa09895df348192cad735a73008
JusupBusurmankulov <busurmankulovzusup@gmail.com>
2022-03-17 00:14:51 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,48 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(goWalk(generateRandomAge(),6));
+        System.out.println(goWalk(generateRandomAge(),23));
+        System.out.println(goWalk(generateRandomAge(),12));
+        System.out.println(goWalk(generateRandomAge(),-21));
+        System.out.println(goWalk(generateRandomAge(),-40));
+
+    }
+    public static String goWalk(int ageHuman, int outdoorTemperature) {
+        {
+
+            if (ageHuman >= 20 && generateRandomAge() <= 45 && outdoorTemperature >= -20 && outdoorTemperature <= 30)
+
+                return "Можно идти гулять";
+
+            else if (ageHuman < 20 && outdoorTemperature >= 0 && outdoorTemperature <= 28)
+
+                return "Можно идти гулять";
+
+            else if (ageHuman > 45 && outdoorTemperature >= -10 && outdoorTemperature <= 25)
+
+                return "Можно идти гулять";
+
+            else
+                return "Отсавайтесь дома";
+
+
+        }
+
+
+    }
+    public static int generateRandomAge(){
+        int a = 0;
+        int b = 100;
+        int ageHuman = a + (int) (Math.random() * b);
+return ageHuman;
+
+
+    }
+
+
+
+}
